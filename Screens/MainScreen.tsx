a6f49e97ff31d89df6e23b9e469252c597563fd2
Richard Hoyle <richardhoyle900@gmail.com>
2023-01-26 11:53:58 +0000

    test

--- Screens/MainScreen.tsx
+++ Screens/MainScreen.tsx
@@ -1,40 +1,37 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import { useNavigation, CompositeNavigationProp, NavigationContainer } from '@react-navigation/native'
-import { RootStackParamList } from '../navigator/RootNavigator';
-import { BottomTabStackParamList } from '../navigator/BottomTabNavigator';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import React from 'react';
+import { View, Text, TouchableOpacity } from "react-native";
+import {
+  useNavigation,
+  CompositeNavigationProp,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { BottomTabStackParamList } from "../navigator/BottomTabNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import React from "react";
 
 import { styled } from "nativewind";
 
-
 type MainScreenNavigation = CompositeNavigationProp<
-    BottomTabNavigationProp<BottomTabStackParamList>,
-    NativeStackNavigationProp<RootStackParamList>
->
+  BottomTabNavigationProp<BottomTabStackParamList>,
+  NativeStackNavigationProp<RootStackParamList>
+>;
+const navigation = useNavigation<MainScreenNavigation>();
+const LoginButton = () => {
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate("Auth")}>
+      <Text>Login / SignIn ( click me HELLOGOGOGOG)</Text>
+    </TouchableOpacity>
+  );
+};
 
 const MainScreen = () => {
-
-    const navigation = useNavigation<MainScreenNavigation>();
-
-    const LoginButton = () => {
-        return (
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Auth")}
-          >
-            <Text>Login / SignIn ( click me)</Text>
-          </TouchableOpacity>
-        );
-    };
-
-
   return (
     <View>
-    <LoginButton/>
+      <LoginButton />
       <Text>MainScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default MainScreen
+export default MainScreen;
