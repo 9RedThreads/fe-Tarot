20fb16f003011a0b30765dc6e22fe14184be7ac5
Richard Hoyle <richardhoyle900@gmail.com>
2023-01-27 16:54:38 +0000

    added a next button and created a state of month on myjournal page

--- Screens/MyJournalScreen.tsx
+++ Screens/MyJournalScreen.tsx
@@ -1,27 +1,43 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { useState } from "react";
 import SimpleJournalCard from "../Components/SimpleJournalCard";
 
 const MyJournalScreen = ({ month }) => {
-
   const monthAsNumber = new Date().getMonth();
   const currentYear = new Date().getFullYear();
 
   function daysInThisMonth() {
     return new Date(currentYear, monthAsNumber, 0).getDate();
   }
+    
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   const [currentDaysInMonth, setCurrentDaysInMonth] = useState(daysInThisMonth);
-
-  const months = ["January","February","March","April","May","June","July",
-  "August","September","October","November","December"];
+  const [currentMonth, setCurrentMonth] = useState(months[monthAsNumber]);
 
   return (
     <ScrollView>
-      <Text className="border">{months[monthAsNumber]}</Text>
-      <View className="border flex-row flex-wrap" >
+      <Text className="border">{currentMonth}</Text>
+      <View className="border flex-row flex-wrap justify-evenly">
         <SimpleJournalCard currentDaysInMonth={currentDaysInMonth} />
+        <TouchableOpacity>
+          <Text>Next...</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
+    //on Monday work on this so on press of next rerenders month new month, changes setsCurrentMonthetc
   );
 };
 
