337fec1247cbe051d18666f961eb5b0d7146d670
Bali Birch-Lee <balibirchlee@Balis-MacBook-Air.local>
2023-01-27 15:59:21 +0000

    journal calendar looks okay and links to journal entries

--- Components/SimpleJournalCard.tsx
+++ Components/SimpleJournalCard.tsx
@@ -1,14 +1,27 @@
+import { useNavigation } from "@react-navigation/native";
+import { Text, View, TouchableOpacity } from "react-native";
 import MyJournalScreen from "../Screens/MyJournalScreen";
 
-const SimpleJournalCard = () => {
+type NavigateToJournalEntriesProp =
+  NativeStackNavigationProp<RootStackParamList>;
+
+const SimpleJournalCard = ({currentDaysInMonth}) => {
+
+  const navigation = useNavigation<NavigateToJournalEntriesProp>();
+
   const arrOfDays = [];
   {
-    for (let i = 1; i <= currentMonth; i++) {
+    for (let i = 1; i <= currentDaysInMonth; i++) {
       arrOfDays.push(i);
     }
   }
 
-  return <div>SimpleJournalCard</div>;
+  return(arrOfDays.map((date) => {
+    return(
+    <TouchableOpacity key={date} onPress={() => navigation.navigate("JournalEntries")}> 
+    <Text className="w-20 h-20 border m-1 p-1 justify-center bg-white" >{date}</Text>
+    </TouchableOpacity>)
+  }));
 };
 
 export default SimpleJournalCard;
--- Screens/MyJournalScreen.tsx
+++ Screens/MyJournalScreen.tsx
@@ -1,29 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { useState } from "react";
 import SimpleJournalCard from "../Components/SimpleJournalCard";
 
 const MyJournalScreen = ({ month }) => {
+
+  const monthAsNumber = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
+
   function daysInThisMonth() {
-    const monthAsNumber = new Date().getMonth();
-    const currentYear = new Date().getFullYear();
     return new Date(currentYear, monthAsNumber, 0).getDate();
   }
-  const [currentMonth, setCurrentMonth] = useState(daysInThisMonth);
+  const [currentDaysInMonth, setCurrentDaysInMonth] = useState(daysInThisMonth);
 
-  const arrOfDays = [];
-  {
-    for (let i = 1; i <= currentMonth; i++) {
-      arrOfDays.push(i);
-    }
-  }
+  const months = ["January","February","March","April","May","June","July",
+  "August","September","October","November","December"];
 
   return (
-    <View>
-      <Text>Days in this month are{arrOfDays}</Text>
-      <View>
-        <SimpleJournalCard CurrentMonth={currentMonth} />
+    <ScrollView>
+      <Text className="border">{months[monthAsNumber]}</Text>
+      <View className="border flex-row flex-wrap" >
+        <SimpleJournalCard currentDaysInMonth={currentDaysInMonth} />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
