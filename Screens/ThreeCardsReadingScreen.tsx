70b855d94ebfeb50a2deee80fba49f69fefeb5d2
Sodney-Plusnet <sidneypinsent3@hotmail.co.uk>
2023-01-27 11:06:51 +0000

    3 card Reading Friday

--- Screens/ThreeCardsReadingScreen.tsx
+++ Screens/ThreeCardsReadingScreen.tsx
@@ -1,11 +1,100 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
 import React from 'react'
 
 const ThreeCardsReadingScreen = () => {
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+})
+
+const [intention, setIntention] = React.useState('');
+const [entry, setEntry] = React.useState('');
+const [intentionPressed, setIntentionPressed] = React.useState(false);
+const [readingStyle, setReadingStyle] = React.useState('');
+const [readingStylePressed, setReadingStylePressed] = React.useState(false);
+
+
+// States for each reading style if needed
+
+// const [pasPresFut, setPasPresFut] = React.useState('');
+// const [oppChallAd, setOpChallAd] = React.useState('');
+// const [strWeakGro, setStrWeakGro] = React.useState('');
+
+const ReadingStyle = () =>(
+<View>
+<Text>Please select a reading style</Text>
+<Button
+        title="pastPresentFuture"
+        onPress={() => { setReadingStyle("pastPresentFuture"); setReadingStylePressed(true)}}
+       
+      />
+
+<Button
+      
+        title="opportunitiesChallengesAdvice"
+        onPress={() =>  {setReadingStyle("opportunitiesChallengesAdvice"); setReadingStylePressed(true)}}
+       
+      />
+
+<Button
+        title="strengthsWeaknessesGrowth"
+        onPress={() =>  {setReadingStyle("strengthsWeaknessesGrowth"); setReadingStylePressed(true)}}
+       
+      />
+
+</View>
+)
+
+const EntryForm = ()=> (
+
+  <View>
+<Text>Please type your diary entry</Text>
+<TextInput 
+       placeholder="Please type your diary entry here"
+        style={styles.input}
+        onChangeText={setEntry}
+        value={entry}
+      />
+<Button title="Submit Entry" onPress={ () => { console.log(entry)}}
+/>
+</View>
+)
+
+
+
+
   return (
     <View>
+
+      <View>
       <Text>ThreeCardsReadingScreen</Text>
+
+      <TextInput 
+       placeholder="Please type your intention here..."
+        style={styles.input}
+        onChangeText={setIntention}
+        value={intention}
+      />
+<Button title="Submit Intention" onPress={ () => {setIntentionPressed(true)}}
+/>
+</View>
+
+{intentionPressed? <ReadingStyle/> :null}
+
+{readingStylePressed? <EntryForm/> :null}
+
+
+
+
+
     </View>
+  
+    
   )
 }
 
