3ba62bb8bf1bcd1ac6a39dada782c7c5d0ca5752
Sodney-Plusnet <sidneypinsent3@hotmail.co.uk>
2023-01-31 16:57:30 +0000

    Start of One Card

--- Screens/OneCardReadingScreen.tsx
+++ Screens/OneCardReadingScreen.tsx
@@ -1,5 +1,10 @@
-import { View, Text, TextInput, Button,StyleSheet} from 'react-native'
+import { View, Text, TextInput, Button,StyleSheet, TouchableOpacity, Image, ScrollView, Modal} from 'react-native'
 import React from 'react'
+import { sidsInfo } from '../Tarot-cards/sids-cards'
+
+
+
+  
 
 const OneCardReadingScreen = () => {
 
@@ -10,12 +15,44 @@ const OneCardReadingScreen = () => {
       borderWidth: 1,
       padding: 10,
     },
+    images: {
+      height: 160,
+      width: 90,
+      alignItems: 'center',
+  
+    }
   })
 
   const [entry, setEntry] = React.useState('');
+  const [cardOne, setCardOne] = React.useState(sidsInfo[Math.floor(Math.random() * sidsInfo.length)])
+const [imagePressed, setImagePressed] = React.useState(false);
+const [selectedCard, setSelectedCard] = React.useState(cardOne);
+
+
+
+const [cardOneImage, setCardOneImage ] = React.useState(
+  <TouchableOpacity onPress={() =>{{setImagePressed(true); setSelectedCard(cardOne)};}}>
+  <Image style = {styles.images}   source = {cardOne.image} />
+  </TouchableOpacity>);
+
+
+  
+const ShowSingleCard = () =>(
+<ScrollView>
+
+{cardOneImage}
+
+</ScrollView>
+
+)
 
   return (
+
+
     <View>
+
+<ShowSingleCard/> 
+
     <Text>Please type your diary entry</Text>
     <TextInput 
            placeholder="Please type your diary entry here"
@@ -27,6 +64,43 @@ const OneCardReadingScreen = () => {
       {/* onPress just console logs entry but entry changes state as you type would likely need another function on onPress to post new entry     */}
     <Button title="Submit Entry" onPress={ () => { console.log(entry)}}
     />
+
+<Modal animationType = {"slide"}
+    transparent={false}
+    visible={imagePressed}
+    >
+<ScrollView>
+  <Image style = {styles.images} source = {cardOne.image}/>
+              <Text >
+                   Name : {selectedCard.name}  
+                    Number: {selectedCard.number} 
+        Arcana: {selectedCard.arcana},
+         Suit: {selectedCard.suit},
+         Elemental: {selectedCard.Elemental}
+      
+                  </Text>
+
+                  <View>
+      <Text>Submit Diary</Text>
+
+      <TextInput 
+       placeholder="Please type your diary here..."
+        style={styles.input}
+        onChangeText={setEntry}
+        value={entry}
+      />
+<Button title="Submit Entry" onPress={() =>{console.log(entry)}}
+/>
+</View>
+
+                  
+                  </ScrollView>
+                  <Text
+  onPress={() => {
+    setImagePressed(!imagePressed);}
+  }> Close Card </Text>
+
+    </Modal>
     </View>
   )
 }
