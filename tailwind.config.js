b402c3f11079d8392c853ec07224852a10cf9a8b
Bali Birch-Lee <balibirchlee@Balis-MacBook-Air.local>
2023-02-01 12:11:03 +0000

    updated config file TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,17 @@ module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./Screens/**/*.{js,jsx,ts,tsx}", "./Components/**/*.{js,jsx,ts,tsx}" ],
   theme: {
 
+    colors: {
+      'white': '#F0F5F1',
+      'red': '#A74A28',
+      'blue': '#C7E0F4',
+      'lightGrey': '#C0C1BC',
+      'darkGrey': '#404541'
+    },
+
     
     extend: {
-      colors: {
-        'white': '#F0F5F1',
-        'red': '#A74A28',
-        'blue': '#C7E0F4',
-        'lightGrey': '#C0C1BC',
-        'darkGrey': '#404541'
-      },
+      
       
 
 
